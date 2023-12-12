635325f8bf92bf45672c0419c94ddd7957a33a4b
shruti0101 <shrutiguptacu@gmail.com>
2023-12-12 15:40:03 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish:["mulish","sans-serif"],
+      },
+      colors:{
+        deepBlue: "#02042a",
+         lightBlue: "#2b84ea",
+          lightBlue300: "#4b94ed",
+        lightBlue500:"#0b727",
+        greenLight: "#61cea" ,
+        grayText: "#818597",
+         LightGray: "#e2e2e2",
+        grayBlue: "#344a6c",
+         deepBlueHead:"#162£56",
+          gray2: "#525a76"
+
+      }
+    },
+  },
+  plugins: [],
+}
