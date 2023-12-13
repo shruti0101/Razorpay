76c840330422b3c643aa7dbbb0a7da52045b8652
shruti0101 <shrutiguptacu@gmail.com>
2023-12-13 15:13:42 +0530

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,20 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {
-      fontFamily:{
-        mullish:["mulish","sans-serif"],
+      fontFamily: {
+        mullish: ["mulish", "sans-serif"],
       },
-      colors:{
+      colors: {
         deepBlue: "#02042a",
-         lightBlue: "#2b84ea",
-          lightBlue300: "#4b94ed",
-        lightBlue500:"#0b727",
-        greenLight: "#61cea" ,
+        lightBlue: "#2b84ea",
+        lightBlue300: "#4b94ed",
+        lightBlue500: "#0b727",
+        greenLight: "#61cea",
         grayText: "#818597",
-         LightGray: "#e2e2e2",
+        LightGray: "#e2e2e2",
         grayBlue: "#344a6c",
-         deepBlueHead:"#162£56",
-          gray2: "#525a76"
+        deepBlueHead: "#162£56",
+        gray2: "#525a76"
 
       }
     },
